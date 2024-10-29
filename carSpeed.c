3ed18415a070a6c79e4a05717ce01a6a0a1da7fa
Sharoun Madoya <madoyasharoun@gmail.com>
2024-10-29 19:27:12 +0300

    :bento:completed carspeed

--- carSpeed.c
+++ carSpeed.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
-#define DISTANCE
-#define SPEED
-#define TIME
+
+int carSpeed(){
+    float distance, speed, time;
+
+    printf("Enter the distance traveled (in km): ");
+    scanf("%f", &distance);
+    
+    printf("Enter the time taken (in hours): ");
+    scanf("%f", &time);
+    
+    speed = distance / time;
+    printf("Speed of the car:", speed);
+}
