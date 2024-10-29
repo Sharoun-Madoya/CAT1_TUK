4e5f66378da70a0dd2549ba5c090599caf816a5a
Sharoun Madoya <madoyasharoun@gmail.com>
2024-10-29 19:35:04 +0300

    :tada:completed squares of four integers

---
+++ square.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int squares() {
+    int num1, num2, num3, num4;
+    
+    printf("Enter four integers: ");
+    scanf("%d %d %d %d", &num1, &num2, &num3, &num4);
+    
+    printf("Square of %d: %d\n", num1, num1 * num1);
+    printf("Square of %d: %d\n", num2, num2 * num2);
+    printf("Square of %d: %d\n", num3, num3 * num3);
+    printf("Square of %d: %d\n", num4, num4 * num4);
+    
+    }
