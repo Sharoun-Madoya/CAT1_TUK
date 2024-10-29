4962a8ef277f186ead262a89956d87bae281944e
Sharoun Madoya <madoyasharoun@gmail.com>
2024-10-29 19:24:57 +0300

    :tada:completed cube volume

--- cylinderVolume.c
+++ cylinderVolume.c
@@ -1,3 +1,15 @@
 #include <stdio.h>
 #define pi 3.142
 
+int cylinderVolume(){
+    float radius, height, volume;
+
+    printf("Enter the radius of the cylinder: ");
+    scanf("%f", &radius);
+    
+    printf("Enter the height of the cylinder: ");
+    scanf("%f", &height);
+    
+    volume = pi * radius * radius * height;
+    printf("Volume of the cylinder:", volume);
+}
